1a2b013a49f757c02a18798102131be44addb827
James Nowecki <james.nowecki@minstersoft.co.uk>
2020-08-28 10:18:31 +0100

    up to the end of commit 2, vid 2

---
+++ src/app/component.service.js
@@ -0,0 +1,20 @@
+class ComponentService {
+    constructor() {
+        this.numberOneInput = document.getElementById("numberOne");
+        this.numberTwoInput = document.getElementById("numberTwo");
+        this.addValues = document.getElementById("addValues");
+        this.resultDiv = document.getElementById("result");
+    }
+
+    getInputs() {
+        return [this.numberOneInput.value, this.numberTwoInput.value];
+    }
+
+    setResult(str) {
+        this.resultDiv.innerText = str;
+    }
+
+    onClick(cb) {
+        this.addValues.addEventListener("click", cb);
+    }
+}
